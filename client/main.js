cb30148b10a1f86d8fde9276dfbab9a0125a86e2
Jorge Gallardo <jorge8486@gmail.com>
2017-08-26 12:22:24 -0400

    pull messages from mongodb

--- client/main.js
+++ client/main.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+import {Chat} from './../imports/api/chat';
 
-var messages = [
-  {
-    _id: '1',
-    name: 'Jorge',
-    time: 123,
-    message: 'How you doing?'
-  },
-  {
-    _id: '2',
-    name: 'Dave',
-    time: 124,
-    message: 'Good, how are you?'
-  },
-  {
-    _id: '3',
-    name: 'Jorge',
-    time: 125,
-    message: 'Great!'
-  }
-];
-
-const renderUserMessages = (db) => {
+const renderChat = (db) => {
   return db.map((user) => {
     return (
       <p key={user._id}>{user.name}: {user.message} <i>at {user.time}</i></p>
@@ -32,12 +13,27 @@ const renderUserMessages = (db) => {
 };
 
 Meteor.startup(() => {
-  let jsx = (
-    <div>
-      <h1>Meteor Chat</h1>
-      {renderUserMessages(messages)}
-    </div>
-  );
-
-  ReactDOM.render(jsx, document.getElementById('app'));
+  Tracker.autorun(() => {
+    let messages = Chat.find().fetch();
+    console.log('Messages', messages);
+    let jsx = (
+      <div>
+        <h1>Meteor Chat</h1>
+        {renderChat(messages)}
+        {/* <form>
+          <input type="username" placeholder="Name"/>
+          <input type="message" placeholder="Enter message"/>
+          <button>Submit Message</button>
+        </form> */}
+      </div>
+    );
+  
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+  var time = new Date().toLocaleTimeString();
+  Chat.insert({
+    name: 'Jorge',
+    time,
+    message: 'Hello there!'
+  });
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+import {Chat} from './../imports/api/chat';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
 });
