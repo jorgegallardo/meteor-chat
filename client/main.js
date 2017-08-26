bb788f47910dfddc6201fa008b23940c3b47534d
Jorge Gallardo <jorge8486@gmail.com>
2017-08-26 13:10:55 -0400

    form submit message to mongodb

--- client/main.js
+++ client/main.js
@@ -12,6 +12,18 @@ const renderChat = (db) => {
   });
 };
 
+const handleSubmit = (e) => {
+  let name = e.target.name.value;
+  let time = Date.now();
+  let message = e.target.message.value;
+  e.preventDefault();
+
+  if(name && message) {
+    Chat.insert({name, time, message});
+    e.target.message.value = '';
+  }
+};
+
 Meteor.startup(() => {
   Tracker.autorun(() => {
     let messages = Chat.find().fetch();
@@ -20,20 +32,13 @@ Meteor.startup(() => {
       <div>
         <h1>Meteor Chat</h1>
         {renderChat(messages)}
-        {/* <form>
-          <input type="username" placeholder="Name"/>
-          <input type="message" placeholder="Enter message"/>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="Name"/>
+          <input type="text" name="message" placeholder="Enter message"/>
           <button>Submit Message</button>
-        </form> */}
+        </form>
       </div>
     );
-  
     ReactDOM.render(jsx, document.getElementById('app'));
-  });
-  var time = new Date().toLocaleTimeString();
-  Chat.insert({
-    name: 'Jorge',
-    time,
-    message: 'Hello there!'
   });
 });
